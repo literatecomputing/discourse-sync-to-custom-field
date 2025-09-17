8cf6ca817443cb0c73b19c43b10e7a5beffb92a9
Jay Pfaffman <jay@literatecomputing.com>
2025-09-17 12:15:27 -0500

    Update plugin skeleton with plugin name

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-MyPluginModule::Engine.routes.draw do
+DiscourseSyncToCustomField::Engine.routes.draw do
   get "/examples" => "examples#index"
   # define routes here
 end
 
-Discourse::Application.routes.draw { mount ::MyPluginModule::Engine, at: "my-plugin" }
+Discourse::Application.routes.draw { mount ::DiscourseSyncToCustomField::Engine, at: "discourse-sync-to-custom-field" }
--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# name: discourse-plugin-name
+# name: discourse-sync-to-custom-field
 # about: TODO
 # meta_topic_id: TODO
 # version: 0.0.1
@@ -8,13 +8,13 @@
 # url: TODO
 # required_version: 2.7.0
 
-enabled_site_setting :plugin_name_enabled
+enabled_site_setting :discourse_sync_to_custom_field_enabled
 
-module ::MyPluginModule
-  PLUGIN_NAME = "discourse-plugin-name"
+module ::DiscourseSyncToCustomField
+  PLUGIN_NAME = "discourse-sync-to-custom-field"
 end
 
-require_relative "lib/my_plugin_module/engine"
+require_relative "lib/discourse_sync_to_custom_field/engine"
 
 after_initialize do
   # Code which should run after Rails has finished booting
