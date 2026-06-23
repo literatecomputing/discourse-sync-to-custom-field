b23711d5d00a03026993597b5cb7685dfa291f28
Jay Pfaffman <jay@literatecomputing.com>
2026-06-23 11:07:47 -0500

    use after_commit

--- plugin.rb
+++ plugin.rb
@@ -15,44 +15,33 @@ module ::DiscourseSyncToCustomField
 end
 
 require_relative "lib/discourse_sync_to_custom_field/engine"
-
 after_initialize do
-  # Code which should run after Rails has finished booting
-  self.add_model_callback(UserEmail, :after_save) do
-    if SiteSetting.discourse_sync_to_custom_field_enabled
-      custom_field =
-        UserField.find_by(name: SiteSetting.discourse_sync_to_custom_field_custom_email_field)
-      return if custom_field.nil?
-      if custom_field.present?
-        return if !self.primary?
-        email = self.email
-        user_field_name = "user_field_#{custom_field.id}"
-        ucf = UserCustomField.find_or_create_by(name: user_field_name, user_id: self.user.id)
-        ucf.value = self.email
-        ucf.save
-      end
-    end
+  self.add_model_callback(UserEmail, :after_commit, on: %i[create update]) do
+    next if !SiteSetting.discourse_sync_to_custom_field_enabled
+
+    custom_field =
+      UserField.find_by(name: SiteSetting.discourse_sync_to_custom_field_custom_email_field)
+    next if custom_field.nil?
+    next if !self.primary?
+
+    user_field_name = "user_field_#{custom_field.id}"
+    ucf = UserCustomField.find_or_create_by(name: user_field_name, user_id: self.user_id)
+    ucf.update(value: self.email)
   end
 
-  self.add_model_callback(UserProfile, :after_save) do
-    if SiteSetting.discourse_sync_to_custom_field_enabled
-      puts "....Enabled"
-      custom_field =
-        UserField.find_by(name: SiteSetting.discourse_sync_to_custom_field_custom_location_field)
-      puts "Got custom field #{custom_field.inspect}"
-      return if custom_field.nil?
-      if custom_field.present?
-        return if self.location.blank?
-        # replace state codes in location
-        location = self.location
-        puts "Original location is #{location}"
-        location = DiscourseSyncToCustomField::ReplaceStateCodes.replace_state_codes(location)
-        puts "Setting location to #{location}"
-        user_field_name = "user_field_#{custom_field.id}"
-        ucf = UserCustomField.find_or_create_by(name: user_field_name, user_id: user.id)
-        ucf.value = location
-        ucf.save
-      end
-    end
+  self.add_model_callback(UserProfile, :after_commit, on: %i[create update]) do
+    next if !SiteSetting.discourse_sync_to_custom_field_enabled
+
+    custom_field =
+      UserField.find_by(name: SiteSetting.discourse_sync_to_custom_field_custom_location_field)
+    next if custom_field.nil?
+    next if self.location.blank?
+
+    location = self.location
+    location = DiscourseSyncToCustomField::ReplaceStateCodes.replace_state_codes(location)
+
+    user_field_name = "user_field_#{custom_field.id}"
+    ucf = UserCustomField.find_or_create_by(name: user_field_name, user_id: self.user_id)
+    ucf.update(value: location)
   end
 end
